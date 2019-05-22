95526fe381fd04da7e2456e3cb58223aaec61f0f
Nuthrash <nuthrash@outlook.com>
2019-05-22 17:02:18 +0800

    Modify current version to 0.0.1, force C# language version to C# 7.3. Add many fields content with Minax related properties.

--- MinaxWebTranslator/MinaxWebTranslator.iOS/Properties/AssemblyInfo.cs
+++ MinaxWebTranslator/MinaxWebTranslator.iOS/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MinaxWebTranslator.iOS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle( "MinaxWebTranslator" )]
+[assembly: AssemblyDescription( "Minax Web Translator for iOS" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany( "Minax" )]
 [assembly: AssemblyProduct("MinaxWebTranslator.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright( "Copyright © 2019 nuthrash" )]
+[assembly: AssemblyTrademark( "Minax" )]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion( "0.0.1.0" )]
+[assembly: AssemblyFileVersion( "0.0.1.0" )]
